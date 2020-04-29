9b63dd844db751372d784cc0cb13a0f91adb9cdf
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2020-04-29 17:10:33 +0200

    Add documentation to the measure.ts file

--- Api/Website/src/Api/measure.ts
+++ Api/Website/src/Api/measure.ts
@@ -5,33 +5,31 @@ import { BodyParts } from '../react-app-env';
 
 
 export async function MeasureBodyParts(img: BodyPixInput ,canvas?:HTMLCanvasElement|null):Promise<{borstMaat:number,heupMaat:number}>{
-      console.log("start")
-      console.log(img)
-  
-           const loadAndPredict = async()=> {
+      //console.log("start")
+      //console.log(img)
 
+          
+           const loadAndPredict = async()=> {
+            //it loads the model
             const net = await bodyPix.load({
               architecture: 'ResNet50',
               outputStride: 16
             });
           
-            /**
-             * One of (see documentation below):
-             *   - net.segmentPerson
-             *   - net.segmentPersonParts
-             *   - net.segmentMultiPerson
-             *   - net.segmentMultiPersonParts
-             * See documentation below for details on each method.
-              */
-             
+            //if no image is defined the functions doesn't do anything
             if(img!==null){
+              //The segmentation is calculated on the image
               const segmentation = await net.segmentPersonParts(img);
+
+              //if a canvas if defined it draws the mask on the canvas
               if(canvas!=null){
                 var mask=bodyPix.toColoredPartMask(segmentation)
             
                 bodyPix.drawMask(canvas,img as ImageType,mask)
+                drawSkeleton(segmentation,canvas)
               }
               
+              //This section measures the body parts with the custom functions and returns an object of it
               var includeParts=[2,3,4,5,12,13]
               var heupMaat=mesurePart(segmentation,(parts)=>{
                 var res=Math.round((parts.leftHip.position.y+parts.rightHip.position.y)/2)
@@ -43,21 +41,28 @@ export async function MeasureBodyParts(img: BodyPixInput ,canvas?:HTMLCanvasElem
                 return res
               },includeParts,canvas || undefined)
 
-              if(canvas!=null){
-                drawSkeleton(segmentation,canvas)
-              }
-              console.log(segmentation);
+              //console.log(segmentation);
+
               return {borstMaat:borstMaat,heupMaat:heupMaat}
+
+              
             }else{
               throw new Error("Image is null")
             }
           }
+        
           return await loadAndPredict();
   }
-
+  /**
+   * 
+   * @param mask The mask of the segmentation to draw
+   * @param canvas The canvas to draw the mask on
+   * 
+   * This function draws the skeletion of the mask on the canvas
+   */
   function drawSkeleton(mask:bodyPix.SemanticPartSegmentation,canvas:HTMLCanvasElement){
     var poses =mask.allPoses.sort((a , b) => b.score - a.score  )
-    console.log(poses)
+    //console.log(poses)
     var pose=poses[0]
       pose.keypoints.forEach(value=>{
         drawPoint(canvas,value.position.x,value.position.y,"red")
@@ -65,6 +70,12 @@ export async function MeasureBodyParts(img: BodyPixInput ,canvas?:HTMLCanvasElem
 
   }
 
+
+  /**
+   * This function draws a y-line on the canvas
+   * @param canvas The canvas to draw the line on
+   * @param yLijn The Y-coördinate of the line
+   */
   function drawLine(canvas:HTMLCanvasElement,yLijn:number){
     var ctx=canvas.getContext("2d")
     if(ctx!==null){
@@ -73,61 +84,107 @@ export async function MeasureBodyParts(img: BodyPixInput ,canvas?:HTMLCanvasElem
     ctx?.fillRect(0,yLijn-0.5,canvas.width,1)
   }
 
-  function drawPoint(canvas:HTMLCanvasElement,x:number,y:number,kleur:string){
+  /**
+   * 
+   * @param canvas The canvas to draw the points on
+   * @param x The X-coördinate of the point
+   * @param y The Y-coördinate of the point
+   * @param color The collor of the point
+   */
+  function drawPoint(canvas:HTMLCanvasElement,x:number,y:number,color:string){
     var ctx=canvas.getContext("2d")
     if(ctx!==null){
-      ctx.fillStyle = kleur
+      ctx.fillStyle = color
       ctx?.fillRect(x-2.5,y-2.5,5,5)
     }else{
-      console.log("canvas null")
+      //console.log("canvas null")
     }
   }
 
-  function clacLengte(mask:bodyPix.SemanticPartSegmentation,yLijn:number,mesurePart:number[],canvas?:HTMLCanvasElement):number{
+  /**
+   * This function calculate the length of the parts that needs to get mesured.
+   * It draws a vertical line and marks the points where there is a transition between the mesureparts and the the other parts.
+   * 
+   * @param mask The segmentation data to calculate on
+   * @param yLijn The Y-coördinate of the line function
+   * @param mesureParts The parts that count for the measure
+   * @param canvas The canvas the function draws the outcome
+   * 
+   * @returns The distance between the first and last point 
+   */
+  function clacLengte(mask:bodyPix.SemanticPartSegmentation,yLijn:number,mesureParts:number[],canvas?:HTMLCanvasElement):number{
     var beginPixel=mask.width*yLijn
     var eindPixel=beginPixel+mask.width
     var outlinePixelNumber:number[]=[]
 
-    console.log(beginPixel)
+    //console.log(beginPixel)
 
+    // Here the code cycles througt the point and detects a transition between parts
     for(var i=beginPixel;i<eindPixel;i++){
       var checkPixel=mask.data[i] 
       var nextPixel=mask.data[i+1]
-      console.log(i,checkPixel,nextPixel)
-      if((mesurePart.includes(checkPixel)&&!mesurePart.includes(nextPixel))||(mesurePart.includes(nextPixel)&&!mesurePart.includes(checkPixel))){
+      //console.log(i,checkPixel,nextPixel)
+
+      //It checks if there is a transition between mesureParts a other parts
+      if((mesureParts.includes(checkPixel)&&!mesureParts.includes(nextPixel))||(mesureParts.includes(nextPixel)&&!mesureParts.includes(checkPixel))){
+        //It adds the point to the collection of points
         outlinePixelNumber.push(i)
       }
     }
+
     outlinePixelNumber.sort((a,b)=>b-a)
-    console.log(outlinePixelNumber)
+    //console.log(outlinePixelNumber)
+
+    //if a canvas is defined it draws the transition points
     if(canvas!=undefined){
-      console.log("color green!")
+      //console.log("color green!")
       outlinePixelNumber.forEach(value=>{
         drawPoint(canvas,value%mask.width,yLijn,"darkblue")
       })
     }
+
+    // it calculates the distance between the first and last transtion and returns it
     return outlinePixelNumber[0]-outlinePixelNumber[outlinePixelNumber.length-1]
   }
 
-  function mesurePart(mask:bodyPix.SemanticPartSegmentation,yLijnFunc:(parts:BodyParts)=>number,dectNum:number[],canvas?:HTMLCanvasElement){
+  /**
+   * This function is used to start a measure on a segmetation
+   * It picks the most confident pose and collect the data into an object.
+   * After this it calcutes the Y-coördinate for the line to mesure.
+   * It calculates the 
+   * @param mask The segmentation data to calculate on
+   * @param yLijnFunc This is the fucntion thats creates the y-line coördinate
+   * @param mesurePart The parts that count for the measure
+   * @param canvas The canvas the function draws the outcome
+   * 
+   * @returns The function returns the width of the parts you want to measure
+   */
+  function mesurePart(mask:bodyPix.SemanticPartSegmentation,yLijnFunc:(parts:BodyParts)=>number,mesurePart:number[],canvas?:HTMLCanvasElement){
+    
+    //It selects the most confident pose
     var poses =mask.allPoses.sort((a , b) => b.score - a.score  )
-    console.log(poses)
+    //console.log(poses)
     var pose=poses[0]
+
+    //it puts the data from the pose into as object
     //@ts-ignore
     var poseData:BodyParts={}
-    
     pose.keypoints.forEach((value:Keypoint) =>{
       //@ts-ignore
         poseData[value.part]=value
     })
+    //console.log(poseData)
 
-    console.log(poseData)
+
     var yLijn=yLijnFunc(poseData)
+
+    //if a canvas is defined the y line is drawn on the canvas
     if(canvas!=undefined){
       drawLine(canvas,yLijn)
     }
     
-    console.log(yLijn)
-    var lengte=clacLengte(mask,yLijn,dectNum,canvas)
+    //console.log(yLijn)
+    //it calculates the width between the transitions and return the width
+    var lengte=clacLengte(mask,yLijn,mesurePart,canvas)
     return lengte
   }
