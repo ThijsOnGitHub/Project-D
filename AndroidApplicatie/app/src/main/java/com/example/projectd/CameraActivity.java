8083e8d105e2e0143abf80830263372ba95d9c21
flamefirenut <41998587+flamefirenut@users.noreply.github.com>
2020-05-15 16:31:30 +0200

    takePhoto WIP
    Permission wordt aan het begin van de file gedaan, app crasht als die geen permissions heeft, en vraagt daarna permissions. Moet nog bewogen worden naar MainActivity
    
    takePhoto bijna werkende, maar krijg dit in debug:
    "Unable to issue take picture. Re-queuing image capture request"

--- AndroidApplicatie/app/src/main/java/com/example/projectd/CameraActivity.java
+++ AndroidApplicatie/app/src/main/java/com/example/projectd/CameraActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.core.impl.PreviewConfig;
 import androidx.camera.core.Preview;
@@ -25,7 +27,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.MediaStore;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -33,7 +37,11 @@ import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.File;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -53,15 +61,26 @@ public class CameraActivity extends AppCompatActivity {
         //Assign uit de layout
         mImageView = findViewById(R.id.image_view);
         mMaakFotoBtn = findViewById(R.id.camera_maakFoto_btn);
-        
+
+        //Ask permission
+        if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+
+            //Om toestemming vragen.
+            String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            requestPermissions(permission, PERMISSION_CODE);
+        }
         //request a CameraProvider
         ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        ImageCapture imageCapture =
+                new ImageCapture.Builder().build();
 
         //check for CameraProvider availability
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                bindPreview(cameraProvider);
+                bindPreview(cameraProvider, imageCapture);
             } catch (ExecutionException | InterruptedException e) {
                 // No errors need to be handled for this Future.
                 // This should never be reached.
@@ -70,27 +89,16 @@ public class CameraActivity extends AppCompatActivity {
 
 
         //Op maakFotoKnop gedrukt
-        mMaakFotoBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Indien geen toestemming voor camera en opslag
-                if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
-                        checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-
-                    //Om toestemming vragen.
-                    String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                    requestPermissions(permission, PERMISSION_CODE);
-                }else{
-                    //Heeft toestemming
-                    openCamera();
-                }
-            }
+        mMaakFotoBtn.setOnClickListener(v -> {
+            //Indien geen toestemming voor camera en opslag
+            takePhoto(imageCapture);
         });
 
     }
 
     //Select a camera and bind the life cycle and use cases
-    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider, ImageCapture imageCapture) {
+
         Preview preview = new Preview.Builder()
                 .build();
 
@@ -98,17 +106,34 @@ public class CameraActivity extends AppCompatActivity {
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture, preview);
 
         PreviewView previewView = findViewById(R.id.preview_view);
         preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.getCameraInfo()));
 
     }
 
+    public void takePhoto(ImageCapture imageCapture) {
+        ExecutorService cameraExecutor = Executors.newSingleThreadExecutor();
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(new File(getFilesDir()+ "/" + System.currentTimeMillis() + ".raw")).build();
 
-    private void openCamera() {
 
-
+        imageCapture.takePicture(outputFileOptions, cameraExecutor,
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+                        Looper.prepare();
+                        String msg = "Pic captured at " + getFilesDir().toString();
+                        Toast.makeText(getBaseContext(), msg,Toast.LENGTH_LONG).show();
+                    }
+                    @Override
+                    public void onError(ImageCaptureException error) {
+                        Looper.prepare();
+                        String msg = "Something went wrong";
+                        Toast.makeText(getBaseContext(), msg,Toast.LENGTH_LONG).show();
+                    }
+        });
     }
 
     //Resultaat van gevraagde toestemming afhandelen.
@@ -117,7 +142,7 @@ public class CameraActivity extends AppCompatActivity {
         switch (requestCode){
             case PERMISSION_CODE: {
                 if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    openCamera();
+                    //TakePhoto();
                 }else{
                     Toast.makeText(this, "Benodigde toestemming geweigerd.", Toast.LENGTH_SHORT).show();
                 }
