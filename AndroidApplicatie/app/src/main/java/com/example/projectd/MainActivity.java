1f3b9a0d2006b9cb4ce7a92565a1888164ba5178
flamefirenut <41998587+flamefirenut@users.noreply.github.com>
2020-05-19 15:45:18 +0200

    Design WIP
    -Permissions nu in op OnClick van QR Code
    -Begin design homepage
    -Header weggehaald en fullscreen aangezet in CameraActivity

--- AndroidApplicatie/app/src/main/java/com/example/projectd/MainActivity.java
+++ AndroidApplicatie/app/src/main/java/com/example/projectd/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.projectd;
 
-import android.content.Context;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_CODE = 1000;
+    ImageButton ToQRCodeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.getSupportActionBar().hide();
+        this.setContentView(R.layout.activity_main);
+        ToQRCodeButton = findViewById(R.id.ToQRButton);
+        ToQRCodeButton.setOnClickListener(v -> {
+            //check & ask permission, or open camera
+            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permission, PERMISSION_CODE);
+            }
+            else{
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
