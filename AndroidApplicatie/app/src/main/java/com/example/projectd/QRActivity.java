d036ac22a08ee18df0ba5413107cbbf3ed1594c0
flamefirenut <41998587+flamefirenut@users.noreply.github.com>
2020-05-23 14:32:17 +0200

    Moved buttons & print QR

--- AndroidApplicatie/app/src/main/java/com/example/projectd/QRActivity.java
+++ AndroidApplicatie/app/src/main/java/com/example/projectd/QRActivity.java
@@ -3,14 +3,19 @@ package com.example.projectd;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.print.PrintHelper;
 
 public class QRActivity extends AppCompatActivity {
-    private static final int PERMISSION_CODE = 1000;
+    private static final int CREATE_FILE = 1;
     Button PrintQRButton;
     Button ForwardButton;
     Button BackButton;
@@ -21,7 +26,12 @@ public class QRActivity extends AppCompatActivity {
         this.setContentView(R.layout.activity_qr);
         PrintQRButton = findViewById(R.id.print);
         PrintQRButton.setOnClickListener(v ->{
-            //TODO insert print code here
+            PrintHelper photoPrinter = new PrintHelper(this);
+            photoPrinter.setScaleMode(PrintHelper.SCALE_MODE_FILL);
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.qrcode10cm);
+            photoPrinter.printBitmap("QR code 10 cm", bitmap);
+
         });
 
         Intent settings = new Intent(QRActivity.this, SettingsActivity.class);
@@ -33,5 +43,6 @@ public class QRActivity extends AppCompatActivity {
         BackButton.setOnClickListener(v ->{
             finish();
         });
+
     }
 }
