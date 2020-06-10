526f0cb7952b4f9160100496146079b7fdb8ff2e
flamefirenut <41998587+flamefirenut@users.noreply.github.com>
2020-06-10 16:07:34 +0200

    Update CameraActivity.java

--- AndroidApplicatie/app/src/main/java/com/example/projectd/CameraActivity.java
+++ AndroidApplicatie/app/src/main/java/com/example/projectd/CameraActivity.java
@@ -192,7 +192,6 @@ public class CameraActivity extends AppCompatActivity {
         } else {
             tts.speak("Neem nu een foto van uw zijkant", TextToSpeech.QUEUE_ADD,null,"1");
             updateFeedback();
-            tts.speak("Neem nu een foto van uw zijkant", TextToSpeech.QUEUE_ADD,null,"1");
         }
 
     }
